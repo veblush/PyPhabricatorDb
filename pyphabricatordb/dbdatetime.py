b646ca3d7c2006970e45d91725ac68b6fd590b9b
Esun Kim <veblush@gmail.com>
2014-12-11 16:03:31 +0900

    Fix dbdatetime timezone bug

--- pyphabricatordb/dbdatetime.py
+++ pyphabricatordb/dbdatetime.py
@@ -1,12 +1,12 @@
+from sqlalchemy import types
 from datetime import datetime, timedelta
-from sqlalchemy import Integer, UnicodeText, Float, DateTime, Boolean, types, Table, event
+from time import mktime
 
 class dbdatetime(types.TypeDecorator):
     impl = types.Integer
-    epoch = datetime(1970, 1, 1)
 
     def process_bind_param(self, value, dialect):
-        return int((value - self.epoch).total_seconds())
+        return int(mktime(value.timetuple()))
 
     def process_result_value(self, value, dialect):
         return datetime.fromtimestamp(value)
